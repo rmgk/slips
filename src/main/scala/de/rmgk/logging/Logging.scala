01dde6c7f1e3a34e96b9fccb2b25521878363520
ragnar <ragnar@rmgk.de>
2018-02-13 22:34:28 +0100

    fix level

--- src/main/scala/de/rmgk/logging/Logging.scala
+++ src/main/scala/de/rmgk/logging/Logging.scala
@@ -2,13 +2,14 @@ package de.rmgk.logging
 
 import de.rmgk.logging.Level._
 
-sealed class Level(val value: Int, val name: String)
+
+class Level(final val value: Int, final val prefix: String)
 object Level {
-  object Trace extends Level(0, "Trace")
-  object Debug extends Level(1, "Debug")
-  object Info extends Level(2, "Info ")
-  object Warn extends Level(3, "Warn ")
-  object Error extends Level(4, "Error")
+  object Trace extends Level(0, "Trace: ")
+  object Debug extends Level(1, "Debug: ")
+  object Info extends Level(2, "")
+  object Warn extends Level(3, "Warn : ")
+  object Error extends Level(4, "Error: ")
 }
 
 case class Logger(tag: String = "",
@@ -16,22 +17,23 @@ case class Logger(tag: String = "",
                   tracing: Boolean = true,
                  ) {
   @inline final def trace(m: => Any)(implicit c: Context): Unit = log(Trace, m)
-  @inline final def debug(m: => Any)(implicit c: Context): Unit = log(Trace, m)
-  @inline final def info(m: => Any)(implicit c: Context): Unit = log(Trace, m)
-  @inline final def warn(m: => Any)(implicit c: Context): Unit = log(Trace, m)
-  @inline final def error(m: => Any)(implicit c: Context): Unit = log(Trace, m)
+  @inline final def debug(m: => Any)(implicit c: Context): Unit = log(Debug, m)
+  @inline final def info(m: => Any)(implicit c: Context): Unit = log(Info, m)
+  @inline final def warn(m: => Any)(implicit c: Context): Unit = log(Warn, m)
+  @inline final def error(m: => Any)(implicit c: Context): Unit = log(Error, m)
 
-  @inline final def tracemsg(implicit c: Context): String = {
-    if (tracing) {
-      val fstring = c.file.value
-      val last = fstring.lastIndexOf('/') + 1
-      s".(${fstring.substring(last)}:${c.line.value})"
-    }
-    else ""
+  @inline final def tracemsg(c: Context): String = {
+    if (tracing) makeTrace(c) else ""
+  }
+
+  @inline final def makeTrace(c: Context): String = {
+    val fstring = c.file.value
+    val last = fstring.lastIndexOf('/') + 1
+    s".(${fstring.substring(last)}:${c.line.value})"
   }
 
   @inline final def log(l: Level, m: => Any)(implicit c: Context): Unit =
-    if (l.value >= level.value) Console.println(s"$tag$m$tracemsg")
+    if (l.value >= level.value) Console.println(s"${l.prefix}$tag$m${tracemsg(c)}")
 }
 
 case class Context(file: sourcecode.File, line: sourcecode.Line)
