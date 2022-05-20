49e5cb98e7b88bec33d3146f0cc7974cc68e8438
ragnar <ragnar@rmgk.de>
2022-05-21 00:32:54 +0200

    update dependencies

---
+++ project/Dependencies.scala
@@ -0,0 +1,109 @@
+/* This file is shared between multiple projects
+ * and may contain unused dependencies */
+
+import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
+import sbt._
+import sbt.Keys._
+
+object Dependencies {
+
+  object Versions {
+    val betterFiles        = "3.9.1"
+    val catsCore           = "2.6.1"
+    val circeCore          = "0.14.1"
+    val decline            = "2.2.0"
+    val directories        = "26"
+    val fastparse          = "2.3.3-16-3da4b3"
+    val jetty              = "9.4.46.v20220331"
+    val jol                = "0.16"
+    val jsoniterScalaCore  = "2.13.24"
+    val jsoniterScalaOld   = "2.13.3" // this is the latest version supporting Scala 2.11 and java 8
+    val jsoup              = "1.15.1"
+    val munit              = "0.7.29"
+    val normalizecss       = "8.0.1"
+    val okHttp             = "4.9.3"
+    val pprint             = "0.7.3"
+    val reactiveStreams    = "1.0.3"
+    val retypecheck        = "0.10.0"
+    val scala211           = "2.11.12"
+    val scala212           = "2.12.15"
+    val scala213           = "2.13.8"
+    val scala3             = "3.1.3-RC3"
+    val scalaJavaTime      = "2.3.0"
+    val scalaLoci          = "0.5.0"
+    val scalaSwing         = "3.0.0"
+    val scalacheck         = "1.16.0"
+    val scalactic          = "3.0.0"
+    val scalajsDom         = "2.1.0"
+    val scalatags          = "0.11.1"
+    val scalatest          = "3.2.12"
+    val scalatestpluscheck = "3.2.11.0"
+    val scribe             = "3.8.2"
+    val sourcecode         = "0.2.8"
+    val tomlScala          = "0.2.2"
+    val upickle            = "2.0.0"
+  }
+
+  import Dependencies.{Versions => V}
+
+  val betterFiles        = Def.setting("com.github.pathikrit" %% "better-files" % V.betterFiles)
+  val catsCore           = Def.setting("org.typelevel" %%% "cats-core" % V.catsCore)
+  val decline            = Def.setting("com.monovore" %%% "decline" % V.decline)
+  val directories        = Def.setting("dev.dirs" % "directories" % V.directories)
+  val fastparse          = Def.setting("com.lihaoyi" %%% "fastparse" % V.fastparse)
+  val jetty              = Def.setting("org.eclipse.jetty" % "jetty-rewrite" % V.jetty)
+  val jol                = Def.setting("org.openjdk.jol" % "jol-core" % V.jol)
+  val jsoup              = Def.setting("org.jsoup" % "jsoup" % V.jsoup)
+  val munit              = Def.setting("org.scalameta" %%% "munit" % V.munit % Test)
+  val munitScalacheck    = Def.setting("org.scalameta" %%% "munit-scalacheck" % V.munit % Test)
+  val normalizecss       = Def.setting("org.webjars.npm" % "normalize.css" % V.normalizecss)
+  val okHttp             = Def.setting("com.squareup.okhttp3" % "okhttp" % V.okHttp)
+  val pprint             = Def.setting("com.lihaoyi" %%% "pprint" % V.pprint)
+  val reactiveStreams    = Def.setting("org.reactivestreams" % "reactive-streams" % V.reactiveStreams)
+  val retypecheck        = Def.setting("io.github.scala-loci" %% "retypecheck" % V.retypecheck)
+  val scalacheck         = Def.setting("org.scalacheck" %%% "scalacheck" % V.scalacheck % "test")
+  val scalactic          = Def.setting("org.scalactic" %% "scalactic" % V.scalactic)
+  val scalaJavaTime      = Def.setting("io.github.cquiroz" %%% "scala-java-time" % V.scalaJavaTime)
+  val scalajsDom         = Def.setting("org.scala-js" %%% "scalajs-dom" % V.scalajsDom)
+  val scalaSwing         = Def.setting("org.scala-lang.modules" %% "scala-swing" % V.scalaSwing)
+  val scalatags          = Def.setting("com.lihaoyi" %%% "scalatags" % V.scalatags)
+  val scalatest          = Def.setting("org.scalatest" %%% "scalatest" % V.scalatest % "test")
+  val scalatestpluscheck = Def.setting("org.scalatestplus" %%% "scalacheck-1-15" % V.scalatestpluscheck % "test")
+  val scribe             = Def.setting("com.outr" %%% "scribe" % V.scribe)
+  val scribeSlf4j        = Def.setting("com.outr" %% "scribe-slf4j" % V.scribe)
+  val sourcecode         = Def.setting("com.lihaoyi" %%% "sourcecode" % V.sourcecode)
+  val tomlScala          = Def.setting("tech.sparse" %%% "toml-scala" % V.tomlScala)
+  val upickle            = Def.setting("com.lihaoyi" %% "upickle" % V.upickle)
+
+  val jsoniterScalaAll = Def.setting {
+    val jsoniterVersion = if (Settings.`is 2.11`(scalaVersion.value))
+      V.jsoniterScalaOld
+    else V.jsoniterScalaCore
+    Seq(
+      ("com.github.plokhotnyuk.jsoniter-scala" %%% "jsoniter-scala-core" % jsoniterVersion exclude ("io.github.cquiroz", s"scala-java-time-tzdb_sjs1_${scalaVersion.value.substring(0, 4)}")),
+      "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % jsoniterVersion
+    )
+  }
+
+  val circeAll = Def.setting(Seq("core", "generic", "parser")
+    .map(n => "io.circe" %%% s"circe-$n" % V.circeCore))
+
+  object loci {
+    def generic(n: String): Def.Initialize[sbt.ModuleID] =
+      if (V.scalaLoci.size > 20)
+        Def.setting("com.github.scala-loci.scala-loci" %%% s"scala-loci-$n" % V.scalaLoci)
+      else Def.setting("io.github.scala-loci"          %%% s"scala-loci-$n" % V.scalaLoci)
+
+    val communication = generic("communication")
+    val circe         = generic("serializer-circe")
+    val tcp           = generic("communicator-tcp")
+    val upickle       = generic("serializer-upickle")
+    val jsoniterScala = generic("serializer-jsoniter-scala")
+    val webrtc        = generic("communicator-webrtc")
+    val wsAkka        = generic("communicator-ws-akka")
+    val wsWeb         = generic("communicator-ws-webnative")
+    val wsJavalin     = generic("communicator-ws-javalin")
+    val wsJetty       = Def.setting(Seq(generic("communicator-ws-jetty").value, jetty.value))
+  }
+
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,27 @@
-addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.22")
+/* This file is shared between multiple projects
+ * and may contain unused dependencies */
+
+// scalajs 1.0
+addSbtPlugin("ch.epfl.scala" % "sbt-scalajs-bundler" % "0.20.0") // https://github.com/scalacenter/scalajs-bundler
+addSbtPlugin("org.scala-js"  % "sbt-scalajs"         % "1.10.0")
+libraryDependencies += "org.scala-js" %% "scalajs-env-jsdom-nodejs" % "1.1.0"
+
+// scalanative
+addSbtPlugin("org.scala-native" % "sbt-scala-native" % "0.4.4")
+
+// crossbuilding
+addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject"      % "1.2.0")
+addSbtPlugin("org.portable-scala" % "sbt-scala-native-crossproject" % "1.2.0")
+
+// operations
+addSbtPlugin("com.dwijnand"     % "sbt-dynver"          % "4.1.1") // https://github.com/dwijnand/sbt-dynver
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.8.1") // https://github.com/sbt/sbt-native-packager
+
+// tooling
+addSbtPlugin("org.tpolecat"       % "tut-plugin"  % "0.6.13")
+addSbtPlugin("pl.project13.scala" % "sbt-jmh"     % "0.4.3")
+addSbtPlugin("org.irundaia.sbt"   % "sbt-sassify" % "1.5.1")
+addSbtPlugin("com.github.sbt"     % "sbt-pgp"     % "2.1.2")
+
+// https://github.com/scalameta/sbt-native-image
+addSbtPlugin("org.scalameta" % "sbt-native-image" % "0.3.2")
