b5b65af19ac286ba21807707b46a9093af3fe4d3
ragnar <ragnar@rmgk.de>
2022-05-21 00:59:51 +0200

    formatting

--- src/main/scala/de/rmgk/logging.scala
+++ src/main/scala/de/rmgk/logging.scala
@@ -7,38 +7,34 @@ object logging {
   /** Logger using Strings as tags */
   type Logger = LoggerT[String]
   object Logger {
-    def apply(tag: String = "",
-              level: Level = Trace,
-              logPrinter: LogPrinter[String] = noTracing
-             ): Logger =
+    def apply(tag: String = "", level: Level = Trace, logPrinter: LogPrinter[String] = noTracing): Logger =
       new LoggerT[String](tag, level, logPrinter)
 
-    lazy val tracing: DefaultLogPrinter[String] = new DefaultLogPrinter[String](tracing = true)
+    lazy val tracing: DefaultLogPrinter[String]   = new DefaultLogPrinter[String](tracing = true)
     lazy val noTracing: DefaultLogPrinter[String] = new DefaultLogPrinter[String](tracing = false)
   }
 
-
   /** Convenience methods for logging at certain levels.
     * Tries to inline log calls so level check happens before method call.
-    * @param tag tag to log with, used by the printer
-    * @param level minimum level that gets printed
+    *
+    * @param tag        tag to log with, used by the printer
+    * @param level      minimum level that gets printed
     * @param logPrinter handles printing and formatting the log messages
     * @tparam T tag type passed to the log printer
     */
-  case class LoggerT[T](tag: T,
-                        level: Level = Trace,
-                        logPrinter: LogPrinter[T] = new DefaultLogPrinter[T](tracing = false)
-                       ) {
+  case class LoggerT[T](
+      tag: T,
+      level: Level = Trace,
+      logPrinter: LogPrinter[T] = new DefaultLogPrinter[T](tracing = false)
+  ) {
     @inline final def trace(m: => Any)(implicit c: Context): Unit = log(Trace, m)
     @inline final def debug(m: => Any)(implicit c: Context): Unit = log(Debug, m)
-    @inline final def info(m: => Any)(implicit c: Context): Unit = log(Info, m)
-    @inline final def warn(m: => Any)(implicit c: Context): Unit = log(Warn, m)
+    @inline final def info(m: => Any)(implicit c: Context): Unit  = log(Info, m)
+    @inline final def warn(m: => Any)(implicit c: Context): Unit  = log(Warn, m)
     @inline final def error(m: => Any)(implicit c: Context): Unit = log(Error, m)
-
 
     @inline final def log(l: Level, m: => Any)(implicit c: Context): Unit =
       if (l.value >= level.value) logPrinter.print(tag, l, m, c)
-
 
   }
 
@@ -50,17 +46,16 @@ object logging {
   /** Existing log levels, each has a corresponding method in the [[Logger]] */
   sealed class Level(final val value: Int, final val prefix: String)
   object Level {
-    // @formatter:off
-  object Trace  extends Level(0, "Trace: ")
-  object Debug  extends Level(1, "Debug: ")
-  object Info   extends Level(2, "Info : ")
-  object Warn   extends Level(3, "Warn : ")
-  object Error  extends Level(4, "Error: ")
-  object Silent extends Level(Int.MaxValue, "Silent")
-  // @formatter:on
+    object Trace  extends Level(0, "Trace: ")
+    object Debug  extends Level(1, "Debug: ")
+    object Info   extends Level(2, "Info : ")
+    object Warn   extends Level(3, "Warn : ")
+    object Error  extends Level(4, "Error: ")
+    object Silent extends Level(Int.MaxValue, "Silent")
   }
 
   /** The [[LogPrinter]] decides how messages are formatted
+    *
     * @tparam T type of the tag used by the logger
     */
   trait LogPrinter[T] {
@@ -70,6 +65,7 @@ object logging {
   /** Prints to stdout.
     * Prints the level if it is not [[Info]].
     * Prints non empty tags.
+    *
     * @param tracing add file and line number to log output if true
     * @tparam T type of the tag used by the logger
     */
@@ -94,11 +90,10 @@ object logging {
     def traceline(c: Context): String = {
       if (tracing) {
         val fstring = c.file.value
-        val last = fstring.lastIndexOf('/') + 1
+        val last    = fstring.lastIndexOf('/') + 1
         s".(${fstring.substring(last)}:${c.line.value})"
       } else ""
     }
   }
-
 
 }
--- src/test/scala/tests/Example.scala
+++ src/test/scala/tests/Example.scala
@@ -1,20 +1,18 @@
 package tests
 
+import de.rmgk.logging.{Level, Logger}
 
 object Example {
-
-  import de.rmgk.logging.{Level, Logger}
 
   object Log {
 
     val common: Logger = Logger(tag = "", level = Level.Trace)
-    val Tool: Logger = common.copy(tag = "Tool", level = Level.Info)
-    val Main: Logger = common
-    val Web: Logger = common.copy(tag = "Web")
-    val Store: Logger = common.copy(tag = "IO")
+    val Tool: Logger   = common.copy(tag = "Tool", level = Level.Info)
+    val Main: Logger   = common
+    val Web: Logger    = common.copy(tag = "Web")
+    val Store: Logger  = common.copy(tag = "IO")
     val Server: Logger = common.copy(tag = "Serv", logPrinter = Logger.tracing)
   }
-
 
   Log.Web.warn("download failed")
 
@@ -26,6 +24,7 @@ object Example {
 
   def main(args: Array[String]): Unit = {
     Server
+    ()
   }
 
 }
