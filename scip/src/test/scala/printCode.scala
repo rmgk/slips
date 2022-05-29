5252622c9b08e1f878c38e067b59458bc2931146
ragnar <ragnar@rmgk.de>
2022-05-29 22:16:42 +0200

    no more default compile time code printing

--- scip/src/test/scala/printCode.scala
+++ scip/src/test/scala/printCode.scala
@@ -1,14 +1,14 @@
 import scala.quoted.*
 
-inline def printCode[T](inline expr: T): String =
-  ${ impl('expr) }
+inline def printCode[T](inline expr: T, inline ct: Boolean = false): String =
+  ${ impl('expr, 'ct) }
 
-def impl[T: Type](expr: Expr[T])(using quotes: Quotes): Expr[String] = {
+def impl[T: Type](expr: Expr[T], ct: Expr[Boolean])(using quotes: Quotes): Expr[String] = {
 
   import quotes.reflect.*
   val e = expr.show.toString
   // val t = expr.asTerm.toString
-  println(e)
+  if (ct.value.getOrElse(false)) println(e)
   // println(t)
   Expr(e)
 
