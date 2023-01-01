239a86348e30f7fa81fb8ab818b02435e1ff483e
ragnar <git@rmgk.de>
2023-01-02 00:49:17 +0100

    better handling for empty commands

--- options/src/main/scala/de/rmgk/options.scala
+++ options/src/main/scala/de/rmgk/options.scala
@@ -34,10 +34,10 @@ object options {
 
   }
 
-  class Subcommand[T <: Product](
+  class Subcommand[T](
       inner: T,
       private[options] val transform: OParser[Unit, Any] => OParser[Unit, Any],
-      private[options] val subparser: OParser[_, Any]
+      private[options] val subparser: Option[OParser[_, Any]]
   ) {
     private[options] var isUsed: Boolean = false
 
@@ -45,6 +45,8 @@ object options {
   }
 
   object Subcommand {
+    def empty(transform: OParser[Unit, Any] => OParser[Unit, Any] = identity) =
+      new Subcommand((), transform, None)
     inline def apply[T <: Product](
         inner: T,
         transform: OParser[Unit, Any] => OParser[Unit, Any] = identity
@@ -62,25 +64,25 @@ object options {
       builder: scopt.OParserBuilder[Any],
       acc: Option[scopt.OParser[_, Any]],
       position: Int
-  ): scopt.OParser[_, Args] =
+  ): Option[scopt.OParser[_, Args]] =
 
     def getCurrent = instance.productElement(position)
 
     inline (erasedValue[ArgsT], erasedValue[Labels]) match
-      case (EmptyTuple, EmptyTuple) => acc.get.asInstanceOf[scopt.OParser[_, Args]]
+      case (EmptyTuple, EmptyTuple) => acc.asInstanceOf[Option[scopt.OParser[_, Args]]]
       case (_: (Subcommand[τ] *: rargs), (_: (label *: rlabels))) =>
         val subvalue = getCurrent.asInstanceOf[Subcommand[τ]]
 
-        val childParser = subvalue.subparser
-
         val name = constValue[label].asInstanceOf[String]
 
-        val end = subvalue.transform(builder.cmd(name).action { (_, args) =>
-          val inst = subvalue
-          inst.isUsed = true
-          args
-        }.children(childParser))
-
+        val end = subvalue.transform {
+          val act = builder.cmd(name).action { (_, args) =>
+            val inst = subvalue
+            inst.isUsed = true
+            args
+          }
+          subvalue.subparser.fold(act)(x => act.children(x))
+        }
 
         val nextAcc = acc.fold(end)(a => a ++ end)
 
@@ -111,13 +113,15 @@ object options {
 
         makeParserRec[rargs, rlabels, Args](instance, builder, Some(nextAcc), position + 1)
 
-  inline def makeParser[Args <: Product](instance: Args, init: OParserBuilder[Args] => OParser[Unit, Args] = null)(using pm: Mirror.ProductOf[Args]) =
+  inline def makeParser[Args <: Product](instance: Args, init: OParserBuilder[Args] => OParser[Unit, Args] = null)(using
+      pm: Mirror.ProductOf[Args]
+  ) =
     val builder = scopt.OParser.builder[Args]
     makeParserRec[pm.MirroredElemTypes, pm.MirroredElemLabels, Args](
       instance,
       builder.asInstanceOf,
       Option(init).map(f => f(builder).asInstanceOf[OParser[_, Any]]),
       0
-    )
+    ).get
 
 }
