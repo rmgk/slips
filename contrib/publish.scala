501cfde127fd402e873fd190982296567c702954
ragnar <git@rmgk.de>
2023-06-29 13:18:48 +0200

    update changelog, update release script

--- contrib/publish.scala
+++ contrib/publish.scala
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S scala-cli shebang
 
-//> using scala "3.3.0-RC2"
+//> using scala "3.3.0"
 //> using lib "de.rmgk.slips::script:0.4.8"
 //> using lib "de.rmgk.slips::options:0.4.7"
 
@@ -27,5 +27,5 @@ def publish(args: String*): Unit = {
         platforms.foreach: platform =>
           val command = s"$project$platform/publishSigned"
           println(command)
-          process"sbtn ${command}".runPrint()
+          process"sbt --client ${command}".runPrint()
 }
