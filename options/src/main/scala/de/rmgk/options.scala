836a1e10f59143fb9ff450300b688b6ab33ddf97
ragnar <git@rmgk.de>
2023-07-21 16:01:54 +0200

    move everything around

--- options/src/main/scala/de/rmgk/options.scala
+++ options/src/main/scala/de/rmgk/options.scala
@@ -1,24 +1,24 @@
 package de.rmgk
 
-import de.rmgk.options.ArgumentContext.{ParseException, ParseResult}
 import de.rmgk.resource.{Resource, ResourceContext, collectResources}
 
 import java.nio.file.Path
+import scala.annotation.tailrec
 import scala.reflect.ClassTag
 
 object options:
 
   @FunctionalInterface
-  trait ArgumentParser[T]:
+  trait ArgumentValueParser[T]:
     def apply(str: String): Option[T]
-  object ArgumentParser:
-    given ArgumentParser[Path]                                       = s => Some(Path.of(s).nn)
-    given ArgumentParser[String]                                     = Some.apply
-    given ArgumentParser[Int]                                        = _.toIntOption
-    given ArgumentParser[Long]                                       = _.toLongOption
-    given ArgumentParser[Double]                                     = _.toDoubleOption
-    given ArgumentParser[Boolean]                                    = _ => Some(true)
-    given [T](using p: ArgumentParser[T]): ArgumentParser[Option[T]] = s => p.apply(s).map(Some.apply)
+  object ArgumentValueParser:
+    given ArgumentValueParser[Path]                                            = s => Some(Path.of(s).nn)
+    given ArgumentValueParser[String]                                          = Some.apply
+    given ArgumentValueParser[Int]                                             = _.toIntOption
+    given ArgumentValueParser[Long]                                            = _.toLongOption
+    given ArgumentValueParser[Double]                                          = _.toDoubleOption
+    given ArgumentValueParser[Boolean]                                         = _ => Some(true)
+    given [T](using p: ArgumentValueParser[T]): ArgumentValueParser[Option[T]] = s => p.apply(s).map(Some.apply)
 
   enum Style:
     case Named
@@ -31,7 +31,7 @@ object options:
       description: String = "",
       default: T | Null = null
   )(using
-      val parser: ArgumentParser[T],
+      val parser: ArgumentValueParser[T],
       val ct: ClassTag[T]
   ) extends Resource {
     type Type = T
@@ -43,75 +43,25 @@ object options:
         summon
       )
 
-  inline def parseArguments[Res](parameters: List[String])(inline expr: Res): ParseResult[Res] =
+  inline def argumentParser[Res](inline expr: Res): ArgumentsParser[Res] =
     val (descriptors, handler) =
       collectResources[Res, de.rmgk.options.Argument[_], de.rmgk.options.ArgumentContext](expr)
-    executeParsing(parameters, descriptors, handler)
+    ArgumentsParser(descriptors, handler)
 
-  def executeParsing[Res](
-      parameters: List[String],
-      descriptors: List[Argument[_]],
-      handler: ArgumentContext => Res
-  ): ParseResult[Res] =
-    ArgumentContext.exec(parameters, descriptors, handler)
+  inline def parseArguments[Res](parameters: List[String])(inline expr: Res): ParseResult[Res] =
+    argumentParser(expr).parse(parameters)
 
-  case class ArgumentContext(bound: Map[Argument[_], Any]) extends ResourceContext[Argument[_]] {
-    override def accessResource(res: Argument[_]): res.Type =
-      bound.get(res).orElse(Option(res.default)).map(_.asInstanceOf[res.Type]).getOrElse:
-        throw ParseException(s"required argument »${res.name}« not provided")
-  }
-
-  object ArgumentContext {
-
-    def exec[Res](
-        parameters: List[String],
-        descriptors: List[Argument[_]],
-        handler: ArgumentContext => Res
-    ): ParseResult[Res] =
+  case class ArgumentsParser[Res](descriptors: List[Argument[_]], handler: ArgumentContext => Res):
+    def parse(parameters: List[String]): ParseResult[Res] =
       try
-        val bound = ArgumentContext.rec(parameters, descriptors, Map.empty)
+        val bound = rec(parameters, descriptors, Map.empty)
         ParseResult(Right(handler(ArgumentContext(bound))))
       catch
         case ParseException(msg) =>
           ParseResult(Left(ParseError(descriptors, msg)))
 
-    case class ParseResult[T](inner: Either[ParseError, T]):
-      def printHelp(): Unit =
-        inner match
-          case Left(pe) =>
-            println("commandline arguments:")
-            println(pe.formatHelp.indent(2))
-            println(s"\n  Note: ${pe.msg}")
-          case Right(value) => ()
-
-    case class ParseError(descriptors: List[Argument[_]], msg: String):
-      def formatHelp: String =
-        val ordered =
-          val grouped    = descriptors.groupBy(_.style)
-          val named      = (grouped.getOrElse(Style.Named, Nil) ++ grouped.getOrElse(Style.Flag, Nil)).sortBy(_.name)
-          val positional = grouped.getOrElse(Style.Positional, Nil)
-          named ++ positional
-
-        val lines = ordered.map: desc =>
-          desc.style match
-            case Style.Named      => s"--${desc.name} ${desc.ct.runtimeClass.getSimpleName}"
-            case Style.Positional => s"<${desc.name}: ${desc.ct.runtimeClass.getSimpleName}>"
-            case Style.Flag       => s"--${desc.name}"
-        val maxline = lines.map(_.size).maxOption.getOrElse(0)
-        lines.lazyZip(ordered).map: (l, d) =>
-          val res =
-            if d.description.nonEmpty
-            then s"%-${maxline}s   %s".format(l, d.description)
-            else l
-          if d.default == null
-          then res
-          else s"$res\n  (default: ${d.default})"
-        .mkString("\n")
-      end formatHelp
-
-    case class ParseException(msg: String) extends Exception(msg, null, false, false)
-
-    def rec(
+    @tailrec
+    final def rec(
         remaining: List[String],
         descriptors: List[Argument[_]],
         bound: Map[Argument[_], Any]
@@ -155,6 +105,48 @@ object options:
                     case None => throw ParseException(s"could not parse »$str« as ${arg.ct.runtimeClass.getSimpleName}")
                     case Some(v) =>
                       rec(rest, remdesc, bound.updated(arg, v))
+  end ArgumentsParser
+
+  case class ArgumentContext(bound: Map[Argument[_], Any]) extends ResourceContext[Argument[_]] {
+    override def accessResource(res: Argument[_]): res.Type =
+      bound.get(res).orElse(Option(res.default)).map(_.asInstanceOf[res.Type]).getOrElse:
+        throw ParseException(s"required argument »${res.name}« not provided")
   }
+
+  case class ParseResult[T](inner: Either[ParseError, T]):
+    def printHelp(): Unit =
+      inner match
+        case Left(pe) =>
+          println("commandline arguments:")
+          println(pe.formatHelp.indent(2))
+          println(s"\n  Note: ${pe.msg}")
+        case Right(value) => ()
+
+  case class ParseError(descriptors: List[Argument[_]], msg: String):
+    def formatHelp: String =
+      val ordered =
+        val grouped    = descriptors.groupBy(_.style)
+        val named      = (grouped.getOrElse(Style.Named, Nil) ++ grouped.getOrElse(Style.Flag, Nil)).sortBy(_.name)
+        val positional = grouped.getOrElse(Style.Positional, Nil)
+        named ++ positional
+
+      val lines = ordered.map: desc =>
+        desc.style match
+          case Style.Named      => s"--${desc.name} ${desc.ct.runtimeClass.getSimpleName}"
+          case Style.Positional => s"<${desc.name}: ${desc.ct.runtimeClass.getSimpleName}>"
+          case Style.Flag       => s"--${desc.name}"
+      val maxline = lines.map(_.size).maxOption.getOrElse(0)
+      lines.lazyZip(ordered).map: (l, d) =>
+        val res =
+          if d.description.nonEmpty
+          then s"%-${maxline}s   %s".format(l, d.description)
+          else l
+        if d.default == null
+        then res
+        else s"$res\n  (default: ${d.default})"
+      .mkString("\n")
+    end formatHelp
+
+  case class ParseException(msg: String) extends Exception(msg, null, false, false)
 
 end options
